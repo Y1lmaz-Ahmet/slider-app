186c2a2ec01f7a0d6d1c53e6da1981af258bc31e
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-02-09 16:22:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Slider from "./slider/Slider";
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Slider></Slider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/slider/Slider.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "../GlobalStyles.css";
+const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const data = [
+    {
+      img: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZXhwZW5zaXZlJTIwc3BvcnQlMjBjYXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60",
+      id: 1,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1507767439269-2c64f107e609?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZXhwZW5zaXZlJTIwc3BvcnQlMjBjYXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1400&q=60",
+      id: 2,
+    },
+
+    {
+      img: "https://images.unsplash.com/photo-1474015977340-64a93f54a9f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGV4cGVuc2l2ZSUyMHNwb3J0JTIwY2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+      id: 4,
+    },
+    {
+      img: "https://images.unsplash.com/photo-1504215680853-026ed2a45def?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGV4cGVuc2l2ZSUyMHNwb3J0JTIwY2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60",
+      id: 5,
+    },
+  ];
+  const next = () => {
+    const indexZero = currentIndex === 0;
+    const nextItem = indexZero ? data.length - 1 : currentIndex - 1;
+    setCurrentIndex(nextItem);
+  };
+  const previous = () => {
+    const lastIndex = currentIndex === data.length - 1;
+    const newIndex = lastIndex ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  return (
+    <div>
+      <div className='slider-container'>
+        <img src={data[currentIndex].img} />
+        <button onClick={previous} id='prev'>
+          previous item
+        </button>
+        <button onClick={next} id='next'>
+          next item
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
